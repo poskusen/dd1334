ea98d3533f1a44e034026a545bc06b7e5eb69ca2
OllenBollenBus <olle.oltorp@live.se>
2024-09-04 11:55:54 +0200

    mer jobb på map och continent

---
+++ Classes/continent.py
@@ -0,0 +1,4 @@
+class Continent:
+
+    def __init__(self, name):
+        self.name = name
--- Classes/map.py
+++ Classes/map.py
@@ -1,9 +1,18 @@
-from continent import continent
+from continent import Continent
 
 class Map:
 
-    def _init_(self, popscale, mapsize, riverscale, mountainscale, tempscale, rainscale):
+    def _init_(self, continentscale, popscale, mapsize, riverscale, mountainscale, tempscale, rainscale, continentlist):
+        self.continentscale = continentscale    #Generates a number of continents 1-x Based on a scale of 1-100
         self.popscale = popscale
         self.mapsize = mapsize
         self.riverscale = riverscale
-        self.
+        self.mountainscale = mountainscale
+        self.tempscale = tempscale
+        self.rainscale = rainscale
+
+        self.continentlist = continentlist
+
+    def generatecontinent(self, continentscale): #Or Fetch continent, depends on where the generative process is supposed to be locatied
+
+        return self.continentlist
