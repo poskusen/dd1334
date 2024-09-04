6b3d9b12e19a4030394f031a1d0b5d68b4459518
OllenBollenBus <olle.oltorp@live.se>
2024-09-04 11:40:58 +0200

    Påbörjan av map.py
    Inkluderar attributer som användaren lägger in som påverkar den generativa processen

---
+++ Classes/map.py
@@ -0,0 +1,9 @@
+from continent import continent
+
+class Map:
+
+    def _init_(self, popscale, mapsize, riverscale, mountainscale, tempscale, rainscale):
+        self.popscale = popscale
+        self.mapsize = mapsize
+        self.riverscale = riverscale
+        self.
